4230444627c164d706ca1df198834ade21e563a5
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-04-28 18:05:47 -0300

    fazendo o debug funcionar

---
+++ Aulas_praticas/teste.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//O problema pra isso não tá debugando dentro de um folder do projeto é que práticas tava com acento e o VScode não gosta 
+int main(){
+    int i = 10;
+    printf("Hello World");
+    i = 1;
+    printf("%d", i);
+}
--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main(){
+    int i = 10;
     printf("Hello World");
+    i = 1;
+    printf("%d", i);
 }
