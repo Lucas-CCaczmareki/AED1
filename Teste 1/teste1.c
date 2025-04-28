8f272d7f8004943e7f5fc654938eeba8e1924b45
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-04-28 13:13:42 -0300

    Pasta de teste 1

---
+++ Teste 1/teste1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+
+    printf("Digite a: ");
+    scanf("%d", &a);
+
+    printf("Digite b: ");
+    scanf("%d", &b);
+
+    printf("a + b = %d", a+b);
+}
