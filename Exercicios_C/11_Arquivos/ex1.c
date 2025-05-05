3a94cda7086309a34c5e86b5bb3af6a06c0b65ba
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-05-05 16:24:14 -0300

    exercicio 05/05/25

--- Exercicios_C/11_Arquivos/ex1.c
+++ Exercicios_C/11_Arquivos/ex1.c
@@ -13,21 +13,28 @@ Agora, abra e leia o
 ==================================================
 */
 
-
-//INCOMPLETO, REVISAR O BÁSICO DE ARQUIVOS AMANHA
 int main ( void ) {
-    FILE *arq = fopen("arq.txt", "a+");
+    FILE *arq = fopen("arq.txt", "w+");
     char c = 'a';
 
-    while( c != '0' ) {
+    while( 1 ) {
         printf( "Write a character! (0 to stop writing): ");
-        scanf( " %c", &c );
-        fwrite(&c, sizeof(char), 1, arq);
+        scanf( " %c", &c ); //esse espaço antes do %c impede a leitura do \n
+
+        if( c == '0' ) break;
+
+        //aqui não é necessário usar fgets ou fwrite, pois ambos são pensados para blocos de informações, strings
+            //e temos funções mais compactas e legiveis pra iss como o fputc para colocar caracter dentro do arquivo, e o fgetc pra ler um caractere do arquivo
+        fputc(c, arq);
+        //fwrite(&c, sizeof(char), 1, arq);
     }
 
     rewind(arq);
-    while(fgets(&c, 1, arq) != 'EOF') {
-        printf("%c\n", &c);
+    while( ( c = fgetc(arq) ) != EOF ) {
+        printf("%c\n", c);
     }
+
+    fclose( arq );
+    return 0;
 
 }
