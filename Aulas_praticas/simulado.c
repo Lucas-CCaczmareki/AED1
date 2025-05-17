5e6938fd9b2cb4a5f7845a3017773feb0faa0f8f
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-05-17 19:13:49 -0300

    79 leet code almost done

--- Aulas_praticas/simulado.c
+++ Aulas_praticas/simulado.c
@@ -2,78 +2,165 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+bool exist(char** board, int boardSize, int* boardColSize, char* word);
+bool isAdjacent(char *word, char **board, int l, int c, int w_pos, int boardSize, int* boardColSize, int lastL, int lastC);
+bool insideLimits(int l, int c, int boardSize, int boardColSize);
+bool atLeast2Adj(char **board, int boardSize, int boardColSize, int l, int c, char alvo);
 
-bool isAdjacent(char *word, char **board, int l, int c, int w_pos, int boardSize, int* boardColSize){
-    bool f_isAdj = false;
-    //supondo a posição [1][0]
-    //vai ter que ver se i-1 e i+1 são as letras
-    //vai ter que ver se j-1 e j+1 são as letras
-    //vai ter que tratar caso tenha uma adjacente em duas posições diferentes
 
-    //printf("%s", word);
-    //printf("\n%c", board[0][1]);
+int main() {
+    // Exemplo de tabuleiro
+    char *board[] = {
+        "ABCE",
+        "SFCS",
+        "ADEE"
+    };
 
-    printf("%c", word[w_pos+1]);
-    printf("%c", board[l][c+1]);
+    int boardSize = 3;               // número de linhas
+    int boardColSize = 4;            // número de colunas por linha (fixo neste exemplo)
+    char word[] = "SEE";          // palavra a ser buscada
 
-    //Enquanto a posição anterior a posição recebida não for \0 (não ta no final da string)
-    //Como eu identifico que é adjacente?
-    while( true ){ //isso tudo aqui ainda não tá tratando casos de letras repetidas
-        if(l != 0 && word[w_pos + 1] == board[l - 1][c] ) {
-            isAdjacent(word, board, l-1, c, w_pos++, boardSize, boardColSize);
+    bool found = exist(board, boardSize, &boardColSize, word);
 
-        } else if(l != boardSize && word[w_pos + 1] == board[l + 1][c] ) {
-            printf("entrei");
-            isAdjacent(word, board, l+1, c, w_pos++, boardSize, boardColSize);
-
-        } else if(c != 0 && word[w_pos + 1] == board[l][c - 1] ) {
-            isAdjacent(word, board, l, c-1, w_pos++, boardSize, boardColSize);
-
-        } else if(c != *boardColSize && word[w_pos + 1] == board[l][c + 1] ) {
-            
-
-            //é a run do b que tá dando merda
-            
-            //o problema tá no l e no c que eu to enviando na chamada recursiva
-            isAdjacent(word, board, l, c+1, w_pos++, boardSize, boardColSize);
-
-        } else {
-            //supondo q tudo deu certo e ele só entra aqui no último loop
-            //ou caso não encontre uma adjacente
-            //eu preciso fazer a lógica de return.
-            if(word[w_pos + 1] == '\0'){
-                return true;
-            } else {
-                return false;
-            }
-        }
+    if (found) {
+        printf("\nPalavra encontrada!\n");
+    } else {
+        printf("\nPalavra NAO encontrada.\n");
     }
-    //retorna a posição da letra adjacente
+
+    return 0;
 }
 
+//OK
+bool insideLimits(int l, int c, int boardSize, int boardColSize){
+    return l >= 0 && l < boardSize && c >= 0 && c < boardColSize;
+}
+
+//OK
+bool atLeast2Adj(char **board, int boardSize, int boardColSize, int l, int c, char alvo){
+    //vetores de direção
+    //0 = cima
+    //1 = baixo
+    //2 = esquerda
+    //3 = direita
+    int dl[] = {-1, 1, 0, 0};
+    int dc[] = {0, 0, -1, 1};
+
+    int count = 0;
+
+    //vai testar com as 4 casas adjacentes
+    for (int i = 0; i < 4; i++) {
+        //dl[0] e dc[0] = [-1, 0] que é = a board[l+(-1)][c+0]
+        int nl = l + dl[i];
+        int nc = c + dc[i];
+
+        //se a nossa posição na board existe e é igual o alvo, incrementa count
+        if (insideLimits(nl, nc, boardSize, boardColSize) &&
+            board[nl][nc] == alvo) {
+            count++;
+        }
+    }
+
+    //se count for maior ou = 2, retorna true
+    return count >= 2;
+}
+
+//OK? 
+bool isAdjacent(char *word, char **board, int l, int c, int w_pos, int boardSize, int* boardColSize, 
+    int lastL, int lastC){
+    //vai ter que tratar caso tenha uma adjacente em duas posições diferentes
+    bool f_2adj = false;
+    bool f_adjs[4] = {false};
+    int i;
+
+     //Se o próximo caracter for \0, significa que chegamos ao final da string com sucesso.
+    if(word[w_pos + 1] == '\0'){
+        return true;
+    }
+
+    //vamo fazer uma função útil pra isso vai
+    //verifica se tem pelo menos duas adjacentes válidas
+    f_2adj = atLeast2Adj(board, boardSize, *boardColSize, l, c, word[w_pos+1]);
+    
+    
+    if(f_2adj){
+        //aqui é pensando no pior caso, as 4 adjacentes são iguais
+        //assim, vai executar uma chamada recursiva pra cada adjacente, se todas retornarem falso, voila
+        for( i = 0; i < 4; i++){
+            //Verifica se a letra seguinte está acima, tratando o final do tabuleiro
+            if( i == 0 && l != 0 && word[w_pos + 1] == board[l - 1][c] && word[w_pos + 1] != board[lastL][lastC] ) {
+                f_adjs[i] = isAdjacent(word, board, l-1, c, w_pos+1, boardSize, boardColSize, l, c);
+            }
+
+            //Verifica se a letra seguinte está abaixo, tratando o final do tabuleiro (-1 pq l começa em 0)
+            if( i == 1 && l != (boardSize-1) && word[w_pos + 1] == board[l + 1][c] && word[w_pos + 1] != board[lastL][lastC] ) {
+                f_adjs[i] = isAdjacent(word, board, l+1, c, w_pos+1, boardSize, boardColSize, l, c);
+            }
+
+            //Verifica se a letra seguinte está na esquerda, trata o final do tabuleiro.
+            if( i == 2 && c != 0 && word[w_pos + 1] == board[l][c - 1] && word[w_pos + 1] != board[lastL][lastC] ) {
+                f_adjs[i] = isAdjacent(word, board, l, c-1, w_pos+1, boardSize, boardColSize, l, c);
+            }
+            
+            //Verifica se a letra seguinte está na direita, trata o final do tabuleiro.
+            if( i == 3 && c != (*boardColSize-1) && word[w_pos + 1] == board[l][c + 1] && word[w_pos + 1] != board[lastL][lastC] ) {       
+                f_adjs[i] = isAdjacent(word, board, l, c+1, w_pos+1, boardSize, boardColSize, l, c);
+            }
+        }
+        //percorre o vetor, se todas forem falsas, não tem palavra válida
+        for( i = 0; i < 4; i++){
+            if(f_adjs[i]){
+                return true;
+            }
+        }
+        return false;
+    } else {
+        //Verifica se a letra seguinte está acima, tratando o final do tabuleiro
+        if(l != 0 && word[w_pos + 1] == board[l - 1][c] && word[w_pos + 1] != board[lastL][lastC] ) {
+            return isAdjacent(word, board, l-1, c, w_pos+1, boardSize, boardColSize, l, c);
+        }
+
+        //Verifica se a letra seguinte está abaixo, tratando o final do tabuleiro (-1 pq l começa em 0)
+        if(l != (boardSize-1) && word[w_pos + 1] == board[l + 1][c] && word[w_pos + 1] != board[lastL][lastC] ) {
+            return isAdjacent(word, board, l+1, c, w_pos+1, boardSize, boardColSize, l, c);
+        }
+
+        //Verifica se a letra seguinte está na esquerda, trata o final do tabuleiro.
+        if(c != 0 && word[w_pos + 1] == board[l][c - 1] && word[w_pos + 1] != board[lastL][lastC] ) {
+            return isAdjacent(word, board, l, c-1, w_pos+1, boardSize, boardColSize, l, c);
+        }
+        
+        //Verifica se a letra seguinte está na direita, trata o final do tabuleiro.
+        if(c != (*boardColSize-1) && word[w_pos + 1] == board[l][c + 1] && word[w_pos + 1] != board[lastL][lastC] ) {       
+            return isAdjacent(word, board, l, c+1, w_pos+1, boardSize, boardColSize, l, c);
+        }
+    }
+    
+    //Caso não tenhamos encontrado nada nas casas adjacentes, retorna falso.
+    return false;
+}
+
+
+//OK?
 bool exist(char** board, int boardSize, int* boardColSize, char* word) {
-    //recebe a board mas pq ela é um ponteiro pra ponteiro?
-    //boardSize? n linhas?
-    //boardColSize? m colunas? mas pq é um ponteiro?
-    //recebe a palavra
-
     //dica, esse exercício é mais fácil de resolver com recursão
-
     int l, c, w_pos = 0;
+    bool isExistent = false;
 
-    //printf("%s", word);
-    //printf("\n%c", board[0][1]);
-
-    //percorre a matriz na horizontal,
+    //percorre a matriz na horizontal até encontrar a primeira letra da palavra
     for( l = 0; l < boardSize; l++) { //percorre as linhas
         for( c = 0; c < *boardColSize; c++) { //percorre as colunas
             if( word[w_pos] == board[l][c] ) {
                 //retorna se a palavra tá ou não na matriz
-                return isAdjacent(word, board, l, c, w_pos, boardSize, boardColSize);
-                //agora eu preciso testar se as adjacentes fazem parte da palavra
-                //e depois se essa que faz parte, tem uma adjacente que faz parte.
+                isExistent = isAdjacent(word, board, l, c, w_pos, boardSize, boardColSize, l, c);
+
+                //se a palavra já existe retorna ela de uma vez e é us guri
+                if(isExistent){
+                    return true;
+                }
             }
         }
     }
-    return false;
+    //Após a matriz inteira, retorna se existe ou não
+    return isExistent;
 }
