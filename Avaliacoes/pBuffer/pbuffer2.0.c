a667b54f73f982e4fa9f5c777dee7fdd70930dd8
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-06-08 21:43:55 -0300

    pbuffer finalizado com drmemory e identação

--- Avaliacoes/pBuffer/pbuffer2.0.c
+++ Avaliacoes/pBuffer/pbuffer2.0.c
@@ -1,3 +1,10 @@
+/*
+DR MEMORY: OK 
+RESTRIÇÕES: Só da uma verificada se na tua função tu n acaba copiando e usando outra área da memória
+COMMENTS & IDENTATION: OK
+VIDEO: NOT DONE
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,18 +15,20 @@
 #define controlRegion   0
 #define dataRegion      200
 
-#define OPT (int *)pBuffer + controlRegion
-#define TOTALSIZE (int *)pBuffer + 1
-#define USEDSIZE (int *)pBuffer + 2
-#define MEMAMOUNT (int *)pBuffer + 3
-#define TOTALPEOPLE (int *)pBuffer + 4
-#define TEMPNAME (char *)pBuffer + 24
-#define TEMPAGE (char *)pBuffer + 82
-#define TEMPMAIL (char *)pBuffer + 132
+#define OPT             (int *)pBuffer + controlRegion
+#define TOTALSIZE       (int *)pBuffer + 1
+#define USEDSIZE        (int *)pBuffer + 2
+#define MEMAMOUNT       (int *)pBuffer + 3
+#define TOTALPEOPLE     (int *)pBuffer + 4
+#define TEMPNAME        (char *)pBuffer + 24
+#define TEMPAGE         (char *)pBuffer + 82
+#define TEMPMAIL        (char *)pBuffer + 132
+#define GENERICPOINTER  (char *)pBuffer + dataRegion;
+#define ENDDATA         (char *)pBuffer + *usedSize;
 
 char* searchPerson(void** pBuffer, char* tempName, int* usedSize, char* p, char* end, int* totalPeople);
 
-/*
+/* Tabela tamanho em bytes
 ==============================================
                 TABELA TAMANHOS:
 ==============================================
@@ -32,30 +41,37 @@ double:         8 bytes.
 ==============================================
 */
 
-
+/*
+main
+Essa função controla o pBuffer, que representa uma agenda.
+pBuffer é uma área de memória que cresce ou diminui conforme adicionamos ou removemos pessoa na agenda
+O programa segue as restrições de:
+somente um malloc
+nenhuma variável (só ponteiros para dentro de pBuffer)
+*/
 int main( void ) {
-    void *pBuffer = malloc(INITIAL_SIZE);
+    void *pBuffer       = malloc(INITIAL_SIZE);
     
+    //=================================================================
     //Creating control region (this has to be called at every realloc)
-    int *opt = OPT;
-    int *totalSize = TOTALSIZE;
-    int *usedSize = USEDSIZE;
-    int *memAmount = MEMAMOUNT;
-    int *totalPeople = TOTALPEOPLE;
+    int *opt            = OPT;
+    int *totalSize      = TOTALSIZE;
+    int *usedSize       = USEDSIZE;
+    int *memAmount      = MEMAMOUNT;
+    int *totalPeople    = TOTALPEOPLE;
 
-    char *tempName = TEMPNAME;
-    char *tempAge = TEMPAGE;
-    char *tempMail = TEMPMAIL;
+    char *tempName      = TEMPNAME;
+    char *tempAge       = TEMPAGE;
+    char *tempMail      = TEMPMAIL;
 
-    char *p = (char *)pBuffer + dataRegion; //points to the begging of data region
-    char *p2 = (char *)pBuffer + dataRegion; //points to the begging of data region
-    char *end = (char *)pBuffer + *usedSize; //points to the end of data region
+    char *p             = GENERICPOINTER;   //points to the begging of data region
+    char *p2            = GENERICPOINTER; 
+    char *end           = ENDDATA;          //points to the end of data region
     //=================================================================
     //Setting initial values
-    *totalSize = INITIAL_SIZE;
-    *usedSize = dataRegion;
-    *totalPeople = 0;
-
+    *totalSize          = INITIAL_SIZE;
+    *usedSize           = dataRegion;
+    *totalPeople        = 0;
     //=================================================================
 
     //========================== MENU =================================
@@ -73,53 +89,66 @@ int main( void ) {
         switch (*opt) {
         case 1:
             /* ====================== Adicionar pessoa ====================== */
-            getchar();
+            //Possíveis problemas:
+                //os scanf q não limitam tamanho de leitura pode estourar minha estrutura
+
+            getchar();                      //limpa buffer do teclado
             printf("Email: ");
-            scanf("%[^\n]s", tempMail);
+            scanf("%[^\n]s", tempMail);     //lê até achar um \n(enter)
             
-            getchar(); //tira o \n do buffer do teclado
+            getchar();                      //tira o \n do buffer do teclado
             printf("Nome: ");
             scanf("%[^\n]s", tempName);
 
             getchar();
             printf("Idade: ");
-            scanf("%s", tempAge);
+            scanf("%s", tempAge);          
 
+            //Confore se a pessoa a ser adicionada já está na agenda, só executa se ela não estiver
             if(searchPerson(&pBuffer, tempMail, usedSize, p, end, totalPeople) != NULL) {
                 printf("Essa pessoa ja esta na agenda!\n");
+
             } else {
+                //Resets memAmount and calculate based on the size of that person's data
                 *memAmount = 0;
                 *memAmount = strlen(tempName) + 1 + strlen(tempAge) + 1 + strlen(tempMail) + 1;
+
                 //if not enought memory, realocate with more
                 if(!( (*usedSize + *memAmount) <= *totalSize )) {   
                     //increase total memory
                     *totalSize += (*usedSize + *memAmount - *totalSize);
-                    pBuffer = realloc(pBuffer, *totalSize);
+                    pBuffer     = realloc(pBuffer, *totalSize);
                     
-                    //update pointers
-                    opt = OPT;
-                    totalSize = TOTALSIZE;
-                    usedSize = USEDSIZE;
-                    memAmount = MEMAMOUNT;
+                    //update pointers from static area
+                    opt         = OPT;
+                    totalSize   = TOTALSIZE;
+                    usedSize    = USEDSIZE;
+                    memAmount   = MEMAMOUNT;
                     totalPeople = TOTALPEOPLE;
 
-                    tempName = TEMPNAME;
-                    tempAge = TEMPAGE;
-                    tempMail = TEMPMAIL;
+                    tempName    = TEMPNAME;
+                    tempAge     = TEMPAGE;
+                    tempMail    = TEMPMAIL;
+
+                    p           = GENERICPOINTER;
+                    p2          = GENERICPOINTER;
+                    end         = ENDDATA;
                 }
 
                 //points to the next free space
                 char *next = (char *)pBuffer + *usedSize;
                 
+                //copies from static area to date area
                 memcpy(next, tempMail, strlen(tempMail) + 1);
                 next += strlen(tempMail) + 1;
 
-                memcpy(next, tempName, strlen(tempName) + 1); //+1 to capture \0
-                next += strlen(tempName) + 1; //points right after \0
+                memcpy(next, tempName, strlen(tempName) + 1);   //+1 to capture \0
+                next += strlen(tempName) + 1;                   //points right after \0
 
                 memcpy(next, tempAge, strlen(tempAge) + 1);
                 next += strlen(tempAge) + 1;
-
+                
+                //uptade data structure area indicators
                 *usedSize += *memAmount;
                 *totalPeople += 1;
             }
@@ -135,48 +164,52 @@ int main( void ) {
                 printf("Quem voce deseja remover? (digite email) ");
                 scanf("%[^\n]s", tempMail);
                 
-                p = searchPerson(&pBuffer, tempMail, usedSize, p, end, totalPeople);
-                p2 = p;
+                //P points to the begging of the element (person) we want to remove
+                //P2 starts at the same point, but will end pointing right after this element
+                p   = searchPerson(&pBuffer, tempMail, usedSize, p, end, totalPeople);
+                p2  = p;
 
                 if( p2 != NULL ) {
-                    //Agora é o problema: 
-                    //preciso armazenar o tamanho q eu to tirando fora (memAmount)
-                    //preciso de 1 ponteiro(p) pro início, pro final do bloco da pra calcular com o tamanho em byte
-                    //ai copia tudo dps do final do bloco pro início
-                    //e pra finalizar realoca pro tamanho total - o tamahnho disso aqui
 
+                    //Calculates the amount of memory we will free and moves P2 to the end of element
                     *memAmount = 0;
-                    *memAmount += strlen(p2) + 1; //guarda o tamanho do email
+                    *memAmount += strlen(p2) + 1;       //saves the size of the email
                     p2 += strlen(p2) + 1;
-                    *memAmount += strlen(p2) + 1; //guarda o tamanho do nome
+                    *memAmount += strlen(p2) + 1;       //saves the size of the name
                     p2 += strlen(p2) + 1;
-                    *memAmount += strlen(p2) + 1; //guarda o tamanho da idade
+                    *memAmount += strlen(p2) + 1;       //saves the size of the age
                     p2 += strlen(p2) + 1;
 
-                    //OK, aqui eu já garanto os 2 ponteiros e o tamanho exato.
-                    end = (char *)pBuffer + *usedSize;
+                    end = (char *)pBuffer + *usedSize;  //uptade end just to make sure
 
-                    //Vou precisar do tanto de memória do p2 até o end;
-                    memcpy(p, p2, (end - p2)); // copia tudo desde p2 até o fim pra onde tá o p;
+                    //end - p2 gives me the amount of bytes between both, cuz they are both char* (1 byte)
+                    memmove(p, p2, (end - p2));         //moves every memory from "p2" til "end" to were "p" is.
 
-                    //agora só realocar
+                    //Realocates pbuffer based on the amount of memory removed
                     pBuffer = realloc(pBuffer, (*totalSize - *memAmount));
-                    //e recolocar os ponteiros apontando pro lugar certo
-                    opt = OPT;
-                    totalSize = TOTALSIZE;
-                    usedSize = USEDSIZE;
-                    memAmount = MEMAMOUNT;
+
+                    //update pointers from static area
+                    opt         = OPT;
+                    totalSize   = TOTALSIZE;
+                    usedSize    = USEDSIZE;
+                    memAmount   = MEMAMOUNT;
                     totalPeople = TOTALPEOPLE;
 
-                    tempName = TEMPNAME;
-                    tempAge = TEMPAGE;
-                    tempMail = TEMPMAIL;
+                    tempName    = TEMPNAME;
+                    tempAge     = TEMPAGE;
+                    tempMail    = TEMPMAIL;
 
-                    *usedSize -= *memAmount;
-                    *totalSize -= *memAmount;
-                    *totalPeople -= 1;
+                    p           = GENERICPOINTER;
+                    p2          = GENERICPOINTER;
+                    end         = ENDDATA;
+
+                    //update data structure indicators
+                    *usedSize       -= *memAmount;
+                    *totalSize      -= *memAmount;
+                    *totalPeople    -= 1;
+
                 } else {
-                    printf( "Essa pessoa nao esta na agenda!\n" );
+                    printf("Essa pessoa nao esta na agenda!\n");
                     break;
                 }
             }
@@ -215,17 +248,17 @@ int main( void ) {
 
         case 4:
             /* ====================== Listar todas ====================== */
-            p = (char *)pBuffer + dataRegion;
-            end = (char *)pBuffer + *usedSize;
+            p   = GENERICPOINTER;
+            end = ENDDATA;
 
-            if(*totalPeople == 0) { //if there is no people in the agenda
+            if(*totalPeople == 0) {             //if there is no people in the agenda
                 printf("Agenda vazia!\n");
                 break;
             } else {
-                while( p < end ) { //while the memory address is before end
+                while( p < end ) {              //while the memory address is before end
                     printf("--------------------------\n");
-                    printf("Email: %s\n", p); //prints the chars from p til the first \0
-                    p += strlen(p) + 1;      //strlen return the amount of chars til the first \0, so p now points right after \0 (cuz +1)
+                    printf("Email: %s\n", p);   //prints the chars from p til the first \0
+                    p += strlen(p) + 1;         //strlen return the amount of chars til the first \0, so p now points right after \0 (cuz +1)
 
                     printf("Nome: %s\n", p);
                     p += strlen(p) + 1;
@@ -239,7 +272,8 @@ int main( void ) {
             break;
 
         case 5:
-            exit(1);
+            free(pBuffer);  //frees the memory used by pBuffer
+            exit(1);        //finishes the program
             break;
 
         default:
@@ -250,14 +284,20 @@ int main( void ) {
     
 }
 
+/*
+searchPerson
+Receiver a pointer to the pointer that points to the area alocated to pBuffer, so the system doesnt create a copy of that
+This function searches the data area based on a given email.
+Then it return a pointer to that email if found, or NULL if not found
+*/
 char* searchPerson(void** pBuffer, char* tempMail, int* usedSize, char* p, char* end, int* totalPeople) {
-    p = (char *)(*pBuffer) + dataRegion;
+    p   = (char *)(*pBuffer) + dataRegion;
     end = (char *)(*pBuffer) + *usedSize;
 
     if(*totalPeople == 0) {
         return NULL;
     } else {
-        while( p <= end ) {
+        while( p < end ) {
             if(strcmp(tempMail, p) == 0) {
                 return p;
             } else {
@@ -265,7 +305,7 @@ char* searchPerson(void** pBuffer, char* tempMail, int* usedSize, char* p, char*
                 //jumps 3 (mail, name and age) \0 to the next nmail
                 p += strlen(p) + 1;
                 p += strlen(p) + 1;
-                p += strlen(p) + 1; //at here, p points to the next mail
+                p += strlen(p) + 1; //at here, p points to the next mail or to the end of structure
             }
         }
     }
