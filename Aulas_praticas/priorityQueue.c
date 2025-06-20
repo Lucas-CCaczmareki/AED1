0c8305fe2b28eec2a099dc056e927c7427f6c184
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-06-20 18:46:16 -0300

    começando a implementar a heap (priorityQueue)

---
+++ Aulas_praticas/priorityQueue.c
@@ -0,0 +1,128 @@
+/* Building a priority queue using the Min-Heap logic to 
+organize my structure. This is going to be an array-based
+heap with maximum capacity for the sake of my mind
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//---------- Structure definitions ----------
+
+//A struct for a "node" in the priority queue. A generic one
+typedef struct {
+    int value;      //Data
+    int priority;   //The priority of the item
+    //smaller number = higher priority (as we are using min heap)
+} Item;
+
+// Structure for the priority queue itself
+// Using arrays
+typedef struct {
+    Item *array;    // Points to the array storing the heap nodes (elements)
+    int capacity;   // Maximum number of elements the heap can hold
+    int size;       // Current number of elements in the heap
+} PriorityQueue;
+
+// --------- Functions for heap operating ---------
+
+// Get parent index
+int parent(int i) {
+    return (i - 1) / 2;
+}
+
+// Get left child index
+int leftChild(int i) {
+    return (2 * i) + 1;
+} 
+
+// Get right child index
+int rightChild(int i) {
+    return (2 * 1) + 2;
+}
+
+// Swap two items in the heap array
+void swap(Item *a, Item *b) {
+    Item temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// --- Core priority queue functions ---
+
+//Creating a new empty priority queue
+PriorityQueue* createPriorityQueue(int capacity) {
+    PriorityQueue *pq = (PriorityQueue *) malloc(sizeof(PriorityQueue));
+    if( pq == NULL ) {
+        printf("Failed to allocate PriorityQueue structure");
+        //como caralhos o malloc sabe o espaço certinho pro vetor?
+        exit(EXIT_FAILURE);
+    }
+
+    pq->array = (Item *) malloc(sizeof(Item) *capacity);
+    if(pq->array == NULL) {
+        printf("Failed to allocate array for Priority Queue");
+        free(pq); //frees the structure of the PQ if the array cannot be allocated
+        exit(EXIT_FAILURE);
+    }
+
+    pq->capacity = capacity;
+    pq->size = 0;
+    printf("Priority queue created with capacity %d.\n", capacity);
+    return pq;
+}
+
+//Chekgin if the Priority Queue is empty
+int isPQEmpty(PriorityQueue *pq) {
+    return pq->size == 0;
+}
+
+// Check if the priority list is full
+int isPQFull(PriorityQueue *pq) {
+    return pq->size == pq->capacity;
+}
+
+// Helper cuntion to maintain min heap property after insertion
+void heapifyUp(PriorityQueue *pq, int i) {
+    //While the current node is not root and its priority is smaller than its parents
+    while (i != 0 && pq->array[parent(i)].priority > pq->array[i].priority) {
+        swap(&pq->array[i], &pq->array[parent(i)]);
+        i = parent(i);  //move up to the parent index
+    }
+}
+
+// Insert an item into the priority queue
+void insert(PriorityQueue *pq, int value, int priority) {
+    if (isPQFull(pq)) {
+        printf("Priority queue is full. Cannot insert %d (priority %d).\n", value, priority);
+        return;
+    }
+
+    // Create the new item
+    Item newItem;
+    newItem.value = value;
+    newItem.priority = priority;
+
+    // Insert the new item ate the end of the array
+    int i = pq->size;
+    pq->array[i] = newItem;
+    pq->size++;
+
+    // Maintain heap property by moving the new item up
+    heapifyUp(pq, i);
+    printf("Inserted: %d (priority %d).\n", value, priority);
+}
+
+// Helper function to maintain Min-heap property after extraction
+void heapifyDown(PriorityQueue *pq, int i) {
+    int smallest = i;
+    int l = leftChild(i);
+    int r = rightChild(i);
+
+    //If the left child exists and its priority is smaller thatn current smallest
+    
+}
+
+
+
+
