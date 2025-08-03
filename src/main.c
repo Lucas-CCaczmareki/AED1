4da6980b6ad84cf94917cf2ecabb027a69df1f81
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-08-03 14:38:44 -0300

    esqueci os comentario

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@ void buildPascal() {
     for (int n = 0; n < MAX; n++) {
         comb[n][0] = 1; //todo No nC0 = 1 por definição
         for(int k = 1; k <= n; k++) {
+            //Armazena em módulo pra não estourar
             comb[n][k] = (comb[n-1][k-1] + comb[n-1][k]) % MOD;
         }
     }
@@ -31,7 +32,7 @@ long long dfs(long long* nums, int numsSize) {
     long long *right_nodes = (long long*)malloc(sizeof(long long) * numsSize);
 
     
-     l = r = 0;
+    l = r = 0;
     //Percorre o vetor, formando as subarvores mantendo a ordem RELATIVA
     for(int i = 1; i < numsSize; i++) {
         if(nums[i] > root) { 
@@ -44,24 +45,19 @@ long long dfs(long long* nums, int numsSize) {
             l++;
         }
     }   
-    //OK agora eu tenho 2 vetores com a ordem relativa (filhos não podem aparecer antes dos pais) preservada!
 
-    //E agora eu tenho X maneiras de escolher L(ou R) posições entre as numsSize-1 livres
-        //PS, ao escolher L posições, as que sobrarem são preenchidas pelo R.
-    
-    //X é dado pela Combinação, C(numSize-1, L) ou C(numsSize-1, R). 
-        //Pois como numsSize-1 = L + R, a simetria da formula da combinação é mantida
-
-
-    //Aparentemente chamar assim estoura o bagulho todo
-    //long long C = (fat(numsSize - 1)) / (fat(l)*(numsSize - 1 - l)); //Calcula a formula da combinação pra esquerda
-    //Como não da pra ficar calculando esses fatorial por que daria um numero maior do que é possível armazenar
-    //criamos uma matriz com triangulo de pascal pra isso
+    //Calcula de quantas maneiras podemos organizar a subarvore esquerda no espaço que sobrou
+    //Como a formula da combinação é simétrica pra direita funciona igual.
     long long C = comb[numsSize-1][l];
+
+    //Calcula de quantas formas podemos organizar as subarvores das subarvores
     long long left = dfs(left_nodes, l);
     long long right = dfs(right_nodes, r);
+
+    //Multiplica os resultados em modulo pra não estourar o long long
     long long result = (((C * left)%MOD) * right) % MOD;
 
+    //Libera a memória ao final da recursão
     free(left_nodes);
     free(right_nodes);
 
@@ -69,17 +65,22 @@ long long dfs(long long* nums, int numsSize) {
 }
 
 int numOfWays(int* nums, int numsSize) {
+    //Cria o triangulo de pascal para não usar o fatorial da combinação
     buildPascal();
+
+    //Copia o vetor n para outro com formato long long e não dar problema de atribuição
     long long *longNums = malloc(sizeof(long long) * numsSize);
     for (int i = 0; i < numsSize; i++) {
         longNums[i] = nums[i];
     }
 
+    //Retorna o resultado liberando a memória (-1 desconsiderando o vetor dado pelo exercício)
     long long result = dfs(longNums, numsSize) - 1;
     free(longNums);
     return result;
 }
 
+//Main com alguns vetores para teste
 int main() {
     int test1[] = {2, 1, 3};
     int test2[] = {3, 4, 5, 1, 2};
@@ -89,7 +90,9 @@ int main() {
     printf("Teste 1: %d\n", numOfWays(test1, sizeof(test1) / sizeof(int))); // Esperado: 1
     printf("Teste 2: %d\n", numOfWays(test2, sizeof(test2) / sizeof(int))); // Esperado: 5
     printf("Teste 3: %d\n", numOfWays(test3, sizeof(test3) / sizeof(int))); // Esperado: 0
-    printf("Teste 4: %d\n", numOfWays(test4, sizeof(test4) / sizeof(int))); // Varia, depende da estrutura da BST
+    printf("Teste 4: %d\n", numOfWays(test4, sizeof(test4) / sizeof(int))); // Esperado: 19
+
+    //system("pause");
 
     return 0;
 }
