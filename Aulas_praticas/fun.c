d05a37ba8acffb749ff613733136cc0039b60cf2
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-09-04 09:47:47 -0300

    atualizando compilador pra 64 bits

--- Aulas_praticas/fun.c
+++ Aulas_praticas/fun.c
@@ -3,16 +3,14 @@
 
 int fun(int n){
     int count = 0, i, j;
-    for (i = n; i > 0; i /= 2) {
-        for (j = 0; j < i; j++){ 
+    for (i = n; i > 0; i /= 2)
+        for (j = 0; j < i; j++)
             count += 1;
             printf("%d\n", count);
-        }
-    }   
     return count;
 }
 
 int main () {
     fun(10);
-    system("pause");
 }
+
